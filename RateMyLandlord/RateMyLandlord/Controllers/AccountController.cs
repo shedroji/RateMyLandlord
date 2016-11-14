e4d0a9a4c8237a85fe90b72a4737e5f111ac231f
Jake Shedroff <shedroji@outlook.com>
2016-11-14 15:10:42 -0500

    added login page and controller. waiting for DB

---
+++ RateMyLandlord/RateMyLandlord/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RateMyLandlord.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return this.RedirectToAction("Login");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            //Validate username ans password is passed
+            
+            //open DB connection
+            
+            //query for user based on username and password
+
+            //if invalid send error
+            
+            //valid, redirect to user profile            
+        }
+    }
+}
