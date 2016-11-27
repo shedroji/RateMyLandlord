2c9ef6e9b810821f8fc2c0395aa3240a4d59c253
Spencer <spencerdean087@gmail.com>
2016-11-27 09:46:54 -0500

    Added wiring for Contact page Added wiring for Landlords, Properties, and Rate pages Added necessary shells for Rate and Landlords pages

---
+++ RateMyLandlord/RateMyLandlord/Controllers/RateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RateMyLandlord.Controllers
+{
+    public class RateController : Controller
+    {
+        // GET: Rate
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- RateMyLandlord/RateMyLandlord/Models/Data/RateMyLandlordDbContext.cs
+++ RateMyLandlord/RateMyLandlord/Models/Data/RateMyLandlordDbContext.cs
@@ -10,5 +10,7 @@ namespace RateMyLandlord.Models.Data
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Property> Properties { get; set; }
+
+        public System.Data.Entity.DbSet<RateMyLandlord.Models.ViewModels.Property.PropertyViewModel> PropertyViewModels { get; set; }
     }
 }
