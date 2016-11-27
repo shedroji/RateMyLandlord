1907a51680bd0bad498218d5433e8ca6e7c60a2d
Jake Shedroff <shedroji@outlook.com>
2016-11-27 10:03:10 -0500

    Added Landlord controller

---
+++ RateMyLandlord/RateMyLandlord/Controllers/LandlordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RateMyLandlord.Controllers
+{
+    public class LandlordController : Controller
+    {
+        // GET: Landlord
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
