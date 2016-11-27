d40f9a10739e42e951b5e8b6b8a2b55853511036
Jake Shedroff <shedroji@outlook.com>
2016-11-26 21:00:43 -0500

    Added Search to Navbar that searches users and properties.

---
+++ RateMyLandlord/RateMyLandlord/Models/ViewModels/Search/SearchResultViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RateMyLandlord.Models.Data;
+
+namespace RateMyLandlord.Models.ViewModels.Search
+{
+    public class SearchResultViewModel
+    {
+        public SearchResultViewModel(Models.Data.Property p)
+        {
+            this.Id = p.Id;
+            this.DisplayText = p.Name;
+            this.DisplaySubText = p.Building + " " + p.Unit + " " + p.Street + " " + p.City;
+            this.ResultType = nameof(Property);
+        }
+
+        public SearchResultViewModel(User u)
+        {
+            this.Id = u.Id;
+            this.DisplayText = u.FirstName + " " + u.LastName;
+            this.DisplaySubText = (u.IsLandlord) ? "Landlord" : "";
+            this.ResultType = nameof(User);
+        }
+
+
+        public int Id { get; set; }
+        public string DisplayText { get; set; }
+        public string DisplaySubText { get; set; }
+        public string ResultType { get; set; }
+    }
+}
