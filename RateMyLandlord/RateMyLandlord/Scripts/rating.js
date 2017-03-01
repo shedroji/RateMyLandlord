471e16be75fdb7c733dd55e65c5afb0253f7cf2b
Jake Shedroff <shedroji@mail.uc.edu>
2017-03-01 11:52:33 -0500

    added bootstrap rating package

---
+++ RateMyLandlord/RateMyLandlord/Scripts/rating.js
@@ -0,0 +1,10 @@
+﻿var configFontAwesome = {
+    custom: {
+        families: ['fontawesome'],
+        urls: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css']
+    },
+    fontactive: function () {
+        $('.rateit-fa').rateit();
+    }
+};
+WebFont.load(configFontAwesome);
