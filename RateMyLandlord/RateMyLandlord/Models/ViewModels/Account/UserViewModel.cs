ba7b56ca8e06cf6031974ad711faf6c44a4e87d6
Jake Shedroff <shedroji@mail.uc.edu>
2017-04-06 20:23:57 -0400

    Landlord pages pulling data from db.

---
+++ RateMyLandlord/RateMyLandlord/Models/ViewModels/Account/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RateMyLandlord.Models.Data;
+
+namespace RateMyLandlord.Models.ViewModels.Account
+{
+    public class UserViewModel
+    {
+        private User item;
+
+        public UserViewModel(User item)
+        {
+            this.Id = item.Id;
+            this.FirstName = item.FirstName;
+            this.LastName = item.LastName;
+            this.Username = item.Username;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+
+    }
+}
